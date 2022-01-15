1bc25d4d608fcf92e2da136c8fe477455528053b
aditya <adityaajmera89@gmail.com>
2022-01-15 11:19:19 +0530

    Initial project add.

---
+++ src/main/java/com/example/petclinix/PetClinixApplication.java
@@ -0,0 +1,13 @@
+package com.example.petclinix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petclinix/PetClinixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petclinix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
