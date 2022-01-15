a362e25d0c3d1be00e44a026a67ee124a420b1f5
aditya <adityaajmera89@gmail.com>
2022-01-15 21:01:26 +0530

    map based service implementation

---
+++ pet-clinix-data/src/main/java/org/learning/petclinix/data/service/map/OwnerServiceMap.java
@@ -0,0 +1,40 @@
+package org.learning.petclinix.data.service.map;
+
+import org.learning.petclinix.data.model.Owner;
+import org.learning.petclinix.data.service.OwnerService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return super.findAll().stream()
+                .filter(owner -> lastName.equalsIgnoreCase(owner.getLastName())).findFirst()
+                .orElseGet(null);
+    }
+}
