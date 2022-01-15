1424186582ef80c4a5dfac1e990869b709485600
aditya <adityaajmera89@gmail.com>
2022-01-15 20:37:34 +0530

    refactor services to base service

--- pet-clinix-data/src/main/java/org/learning/petclinix/data/service/OwnerService.java
+++ pet-clinix-data/src/main/java/org/learning/petclinix/data/service/OwnerService.java
@@ -2,11 +2,6 @@ package org.learning.petclinix.data.service;
 
 import org.learning.petclinix.data.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-    Owner findById(Long id);
-    Set<Owner> findAll();
-    Owner save(Owner owner);
+public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String lastName);
 }
--- pet-clinix-data/src/main/java/org/learning/petclinix/data/service/VetService.java
+++ pet-clinix-data/src/main/java/org/learning/petclinix/data/service/VetService.java
@@ -2,10 +2,5 @@ package org.learning.petclinix.data.service;
 
 import org.learning.petclinix.data.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet save(Vet vet);
-    Vet findById(Long id);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
