d65ebcd73df688499663ca2c7ec34e0e36b03131
aditya <adityaajmera89@gmail.com>
2022-01-16 19:09:59 +0530

    adding speciality services

---
+++ pet-clinix-data/src/main/java/org/learning/petclinix/data/service/SpecialityService.java
@@ -0,0 +1,6 @@
+package org.learning.petclinix.data.service;
+
+import org.learning.petclinix.data.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinix-data/src/main/java/org/learning/petclinix/data/service/map/SpecialityServiceMap.java
@@ -0,0 +1,36 @@
+package org.learning.petclinix.data.service.map;
+
+import org.learning.petclinix.data.model.Speciality;
+import org.learning.petclinix.data.service.SpecialityService;
+import org.learning.petclinix.data.service.map.AbstractMapService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
