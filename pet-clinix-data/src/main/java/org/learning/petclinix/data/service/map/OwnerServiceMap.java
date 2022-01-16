aabb0141e0c6c814e6a0544040f06f2127160b6f
aditya <adityaajmera89@gmail.com>
2022-01-16 17:13:07 +0530

    updating owner relation to pet in data loader

--- pet-clinix-data/src/main/java/org/learning/petclinix/data/service/map/OwnerServiceMap.java
+++ pet-clinix-data/src/main/java/org/learning/petclinix/data/service/map/OwnerServiceMap.java
@@ -1,13 +1,24 @@
 package org.learning.petclinix.data.service.map;
 
 import org.learning.petclinix.data.model.Owner;
+import org.learning.petclinix.data.model.Pet;
 import org.learning.petclinix.data.service.OwnerService;
+import org.learning.petclinix.data.service.PetService;
+import org.learning.petclinix.data.service.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -25,7 +36,27 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner owner) {
-        return super.save(owner);
+        if (owner != null) {
+            if (owner.getPets() != null) {
+                owner.getPets().forEach(pet -> {
+                    if (pet.getPetType() == null) {
+                        throw new RuntimeException("Pet type is required");
+                    } else {
+                        if (pet.getPetType().getId() == null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    }
+
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(owner);
+        } else {
+            return null;
+        }
     }
 
     @Override
--- pet-clinix-web/src/main/java/org/learning/petclinix/web/bootstrap/Dataloader.java
+++ pet-clinix-web/src/main/java/org/learning/petclinix/web/bootstrap/Dataloader.java
@@ -1,6 +1,7 @@
 package org.learning.petclinix.web.bootstrap;
 
 import org.learning.petclinix.data.model.Owner;
+import org.learning.petclinix.data.model.Pet;
 import org.learning.petclinix.data.model.PetType;
 import org.learning.petclinix.data.model.Vet;
 import org.learning.petclinix.data.service.OwnerService;
@@ -8,6 +9,8 @@ import org.learning.petclinix.data.service.PetTypeService;
 import org.learning.petclinix.data.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class Dataloader implements CommandLineRunner {
@@ -35,10 +38,32 @@ public class Dataloader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Aditya");
         owner1.setLastName("Ajmera");
+        owner1.setAddress("Gumasta nagar");
+        owner1.setCity("Indore");
+        owner1.setTelephone("1342341344");
+
+        Pet dog1 = new Pet();
+        dog1.setPetType(dogType);
+        dog1.setName("Ruby");
+        dog1.setBirthDate(LocalDate.now());
+        dog1.setOwner(owner1);
+
+        owner1.getPets().add(dog1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Anika");
+        owner2.setAddress("Gumasta nagar");
+        owner2.setCity("Indore");
+        owner2.setTelephone("1342341344");
         owner2.setLastName("AJmera");
+
+        Pet cat1 = new Pet();
+        cat1.setPetType(catType);
+        cat1.setOwner(owner2);
+        cat1.setBirthDate(LocalDate.now());
+        cat1.setName("Lucy");
+
+        owner2.getPets().add(cat1);
 
         ownerService.save(owner1);
         ownerService.save(owner2);
