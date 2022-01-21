a005348bce8a6ec6dbb546dde13ecac60a184f4e
aditya <adityaajmera89@gmail.com>
2022-01-21 12:31:53 +0530

    create visit service and map implementation.

---
+++ pet-clinix-data/src/main/java/org/learning/petclinix/data/service/VisitService.java
@@ -0,0 +1,6 @@
+package org.learning.petclinix.data.service;
+
+import org.learning.petclinix.data.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinix-data/src/main/java/org/learning/petclinix/data/service/map/VisitServiceMap.java
@@ -0,0 +1,35 @@
+package org.learning.petclinix.data.service.map;
+
+import org.learning.petclinix.data.model.Visit;
+import org.learning.petclinix.data.service.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
