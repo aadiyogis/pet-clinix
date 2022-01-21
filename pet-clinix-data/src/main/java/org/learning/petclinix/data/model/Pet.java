c6d6c58493660c0505bb43d9775f1d26479a2557
aditya <adityaajmera89@gmail.com>
2022-01-21 10:50:17 +0530

    mapping for owners entity

--- pet-clinix-data/src/main/java/org/learning/petclinix/data/model/Pet.java
+++ pet-clinix-data/src/main/java/org/learning/petclinix/data/model/Pet.java
@@ -1,11 +1,23 @@
 package org.learning.petclinix.data.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     public Owner getOwner() {
